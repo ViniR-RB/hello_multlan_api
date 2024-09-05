0e2046bd713a537e7ed76b72880d692e64a2189e
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2024-09-04 21:08:39 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
