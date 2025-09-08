60c042976511a0b99a62df5c900e1a1dd2fee53f
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-08 16:36:39 -0300

    feat: create box domain entity and rules for domain

--- src/modules/box/domain/entities/box.entity.ts
+++ src/modules/box/domain/entities/box.entity.ts
@@ -1,10 +1,111 @@
+import { BoxZone } from '@/modules/box/domain/entities/box_zone_enum';
+import BoxDomainException from '@/modules/box/exceptions/box_domain.exception';
+import { randomUUID } from 'crypto';
+
 interface BoxEntityProps {
   id?: string;
-
+  latitude: number;
+  longitude: number;
+  freeSpace: number;
+  filledSpace: number;
+  signal: number;
+  zone: BoxZone;
+  routeId?: string | null;
+  imageUrl?: string | null;
+  note?: string | null;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 export default class BoxEntity {
-  constructor() {}
+  private constructor(private readonly props: BoxEntityProps) {
+    this.props = {
+      id: props.id ?? randomUUID().toString(),
+      latitude: props.latitude,
+      longitude: props.longitude,
+      freeSpace: props.freeSpace,
+      filledSpace: props.filledSpace,
+      signal: props.signal,
+      zone: props.zone,
+      routeId: props.routeId ?? null,
+      imageUrl: props.imageUrl ?? null,
+      note: props.note ?? null,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+    };
+  }
+
+  static validate(props: BoxEntityProps) {
+    if (props.filledSpace > props.freeSpace) {
+      throw new BoxDomainException(
+        'Filled space cannot be greater than free space',
+      );
+    }
+    if (props.zone === undefined) {
+      throw new BoxDomainException('Zone is required');
+    }
+  }
+  static create(props: BoxEntityProps) {
+    this.validate(props);
+    return new BoxEntity(props);
+  }
+
+  static fromData(props: BoxEntityProps) {
+    return new BoxEntity(props);
+  }
+
+  get id() {
+    return this.props.id!;
+  }
+
+  get latitude() {
+    return this.props.latitude;
+  }
+  get longitude() {
+    return this.props.longitude;
+  }
+  get freeSpace() {
+    return this.props.freeSpace;
+  }
+  get filledSpace() {
+    return this.props.filledSpace;
+  }
+  get signal() {
+    return this.props.signal;
+  }
+  get zone() {
+    return this.props.zone;
+  }
+  get routeId() {
+    return this.props.routeId || null;
+  }
+  get imageUrl() {
+    return this.props.imageUrl || null;
+  }
+  get note() {
+    return this.props.note || null;
+  }
+  get createdAt() {
+    return this.props.createdAt!;
+  }
+  get updatedAt() {
+    return this.props.updatedAt!;
+  }
+
+  toObject() {
+    return {
+      id: this.props.id,
+      latitude: this.props.latitude,
+      longitude: this.props.longitude,
+      freeSpace: this.props.freeSpace,
+      filledSpace: this.props.filledSpace,
+      signal: this.props.signal,
+      zone: this.props.zone,
+      routeId: this.props.routeId,
+      imageUrl: this.props.imageUrl,
+      note: this.props.note,
+      createdAt: this.props.createdAt,
+      updatedAt: this.props.updatedAt,
+    };
+  }
 }
